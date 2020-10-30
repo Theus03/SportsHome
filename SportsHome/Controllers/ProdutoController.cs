a2f75e80067b7d9838adc450b1fa2f4a08fc1b88
Matheus <matheusssleite@outlook.com>
2020-10-29 21:14:39 -0300

    Creation of View Produtos

--- SportsHome/Controllers/ProdutoController.cs
+++ SportsHome/Controllers/ProdutoController.cs
@@ -13,10 +13,8 @@ namespace SportsHome.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Produtos()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
